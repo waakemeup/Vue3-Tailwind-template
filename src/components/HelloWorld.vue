dc718e75385b86cb984ec1512a2cf409ed5b8130
Ruby <LIU10032@163.com>
2021-08-21 12:45:12 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <button class="bg-blue-500 hover:bg-blue-800 text-white" type="button" @click="state.count++">
+    count is: {{ state.count }}
+  </button>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
